b891f32f3fcdbcc2c7bda12bf2d1ab535a829e23
userkilz <141530435+userkilz@users.noreply.github.com>
2023-08-11 20:13:05 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2245,7 +2245,7 @@ class PlayState extends MusicBeatState
 	public function updateScore(miss:Bool = false)
 	{
 		scoreTxt.text = 'Score: ' + songScore
-		+ ' | Ported By userkilz | Misses: ' + songMisses
+		+ ' | Ported by userkilz | Misses: ' + songMisses
 		+ ' | Rating: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
 
