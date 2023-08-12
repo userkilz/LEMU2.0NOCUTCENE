42481044ebf509f9a977c08e0b6f712d5f34dcc6
userkilz <141530435+userkilz@users.noreply.github.com>
2023-08-11 21:57:40 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -82,7 +82,7 @@ class CreditsState extends MusicBeatState
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color 
 			['Mod Port By'],
-			['Glauber_BFSoulEvilYTOFCBR, 'Glauber_BFSoulEvilYTOFCBR', 'Eu Portei Este Mod Incrivel','https://youtube.com/@Glauber_BFSoulEvilYTOFCBR', 'FFFFFF'],
+			['GlauberBFSoul, 'GlauberBFSoul', 'Eu Portei Este Mod Incrivel','https://youtube.com/@Glauber_BFSoulEvilYTOFCBR', 'FFFFFF'],
 			[''],
 			['Creator Mod By'],
 			['lemu, 'lemu', 'O Criador Do Mod','https://youtube.com/@LEMU_BF_SKR', 'FFFFFF'],
